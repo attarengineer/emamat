17a601cdc1efa8a7df18eca388667ff2444bf9df
unknown <AlirezA@1-Emamat-PC.Emamat.local>
2021-09-14 08:28:59 -0700

    edit template and add about.html and contact.html

--- website/urls.py
+++ website/urls.py
@@ -5,8 +5,8 @@ app_name = 'website'
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('about', about_view, name='about'),
     path('contact', contact_view, name='contact'),
-    path('about', about_view),
     path('http_test', http_test),
     path('json_test', json_test),
 ]
--- website/views.py
+++ website/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 
+def index_view(request):
+    return render(request, '../templates/website/index.html')
+
+
+def about_view(request):
+    return render(request, '../templates/website/about.html')
+
+
+def contact_view(request):
+    return render(request, '../templates/website/contact.html')
+
+
 def http_test(request):
     return HttpResponse('<h1>salam bacheha</h1>')
 
@@ -10,17 +22,4 @@ def json_test(request):
     return JsonResponse({'name': 'alireza'})
 
 
-def index_view(request):
-    return render(request, '../templates/website/index.html')
 
-
-def about_view(request):
-    return HttpResponse('<h1>About page</h1>')
-
-
-def contact_view(request):
-    return render(request, '../templates/website/contact.html')
-
-
-def home2_view(request):
-    return render(request, '../templates/website/home-2.html')
