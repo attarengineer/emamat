3cb5fe531afb00f49e89cb792c6924f42e692ec6
AlirezA <engineer.attar@gmail.com>
2021-09-18 00:27:25 +0430

    add single blog in template and add template tag

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog.views import *
 app_name = 'blog'
 
 urlpatterns = [
-    path('', blog_view, name='blog-archive'),
+    path('', blog_archive_view, name='blog-archive'),
+    path('<int:pid>', blog_single_view, name='blog-single'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 
-def blog_view(request):
-    return render(request, '../templates/blog/blog-archive.html')
+def blog_archive_view(request):
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, '../templates/blog/blog-archive.html', context)
+
+
+def blog_single_view(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post': post}
+    return render(request, '../templates/blog/blog-single.html', context)
