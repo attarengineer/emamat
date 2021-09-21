3b0608effbc2770cad9f34825713bec51036c255
unknown <AlirezA@1-Emamat-PC.Emamat.local>
2021-09-21 08:30:05 -0700

    add author template tag

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_archive_view, name='blog-archive'),
     path('<int:pid>', blog_single_view, name='blog-single'),
-    path('category/<str:cat_name>', blog_archive_view, name='category')
+    path('category/<str:cat_name>', blog_archive_view, name='category'),
+    path('author/<str:author_username>', blog_archive_view, name='author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
-def blog_archive_view(request, cat_name=None):
+def blog_archive_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(status=1)
     if cat_name:
         posts = Post.objects.filter(status=1, category__name=cat_name)
+    if author_username:
+        posts = Post.objects.filter(status=1, author__username=author_username)
     context = {'posts': posts}
     return render(request, 'blog/blog-archive.html', context)
 
