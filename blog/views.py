131290b1d3018edfa370615a2105569d8e73d4b2
unknown <engineer.attar@gmail.com>
2021-09-23 13:54:31 +0330

    add paginations in views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def blog_archive_view(request, cat_name=None, author_username=None):
@@ -8,6 +9,14 @@ def blog_archive_view(request, cat_name=None, author_username=None):
         posts = Post.objects.filter(status=1, category__name=cat_name)
     if author_username:
         posts = Post.objects.filter(status=1, author__username=author_username)
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog-archive.html', context)
 
@@ -20,5 +29,8 @@ def blog_single_view(request, pid):
 
 def blog_search(request):
     posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
     context = {'posts': posts}
     return render(request, 'blog/blog-archive.html', context)
