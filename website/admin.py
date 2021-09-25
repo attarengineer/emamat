4dd61fe8142c685998141507e5b52e08cf21cd71
unknown <engineer.attar@gmail.com>
2021-09-25 14:56:58 +0330

    add contact form

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from website.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin (admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_date')
